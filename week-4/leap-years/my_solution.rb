9d4e7a885f586796cf5db5ef590196673897aea3
reginafcompton <reginafcompton@gmail.com>
2015-09-10 12:56:54 -0500

    make small edit to leap-year

--- week-4/leap-years/my_solution.rb
+++ week-4/leap-years/my_solution.rb
@@ -5,22 +5,23 @@
 
 # Your Solution Below
 
-# def leap_year?(year)
-#   if (year % 4 == 0)
-#     if (year % 100 == 0)
-#       if (year % 400 == 0)
-#         return true
-#       end
-#         return false
-#       end
-#       return true
-#     end
-#     return false
-# end
+# Solution 1
+def leap_year?(year)
+  if (year % 4 == 0)
+    if (year % 100 == 0)
+      if (year % 400 == 0)
+        return true
+      end
+        return false
+      end
+      return true
+    end
+    return false
+end
 
-# print leap_year?(2015)
+print leap_year?(2015)
 
-
+# Solution 2
 def leap_year?(year)
   if (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))
     return true
