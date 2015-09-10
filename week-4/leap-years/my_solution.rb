762940c6ded5f6b4efb8965a5fa87680c78ad2d3
reginafcompton <reginafcompton@gmail.com>
2015-09-10 12:52:19 -0500

    add solution for leap-year challenge

---
+++ week-4/leap-years/my_solution.rb
@@ -0,0 +1,32 @@
+# Leap Years
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+# def leap_year?(year)
+#   if (year % 4 == 0)
+#     if (year % 100 == 0)
+#       if (year % 400 == 0)
+#         return true
+#       end
+#         return false
+#       end
+#       return true
+#     end
+#     return false
+# end
+
+# print leap_year?(2015)
+
+
+def leap_year?(year)
+  if (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))
+    return true
+  else
+    return false
+  end
+end
+
+
