312d34f1a3e592149294f65f986afa3f4c0b91fb
reginafcompton <reginafcompton@gmail.com>
2015-09-21 19:37:10 -0500

    update solution for GPS 2.3

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -1,35 +1,25 @@
-# Your Names
-# 1)
-# 2)
-
-# We spent [#] hours on this challenge.
-
-# Bakery Serving Size portion calculator.
-
 def serving_size_calc(item_to_make, order_quantity)
   library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
 
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      p error_counter += -1
+    unless library.include?(item_to_make)
+      raise ArgumentError.new("#{item_to_make} is not a valid input")
     end
-  end
 
-  if error_counter > 0
-    raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
+    serving_size = library[item_to_make]
+    serving_size_mod = order_quantity % serving_size
 
-  serving_size = library.values_at(item_to_make)[0]
-  serving_size_mod = order_quantity % serving_size
+    if serving_size_mod == 0
+        return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
 
-  case serving_size_mod
-  when 0
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
-  else
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+    elsif serving_size_mod >= 7
+       p "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}. You have #{serving_size_mod} leftover ingredient. You can make #{serving_size_mod/7} more pie(s) plus #{serving_size_mod % 7} more cookies."
+    elsif serving_size_mod >= 5
+     p "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}. You have #{serving_size_mod} leftover ingredient. You can have #{serving_size_mod/5} more cake(s) plus #{serving_size_mod % 5} more cookies."
+    else
+      p "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}. You have #{serving_size_mod} leftover ingredient. You can have #{serving_size_mod/1} more cookie(s)."
   end
 end
+
 
 p serving_size_calc("pie", 7)
 p serving_size_calc("pie", 8)
@@ -40,4 +30,9 @@ p serving_size_calc("cookie", 10)
 p serving_size_calc("THIS IS AN ERROR", 5)
 
 #  Reflection
+
+What did you learn about making code readable by working on this challenge?
+Did you learn any new methods? What did you learn about them?
+What did you learn about accessing data in hashes?
+What concepts were solidified when working through this challenge?
 
