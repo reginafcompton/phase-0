0e15d5985a37510c1de01a48ba943c87e49ab064
reginafcompton <reginafcompton@gmail.com>
2015-09-21 19:46:26 -0500

    add reflection to gps 2-3

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -12,11 +12,11 @@ def serving_size_calc(item_to_make, order_quantity)
         return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
 
     elsif serving_size_mod >= 7
-       p "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}. You have #{serving_size_mod} leftover ingredient. You can make #{serving_size_mod/7} more pie(s) plus #{serving_size_mod % 7} more cookies."
+       p "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}. You have #{serving_size_mod} leftover ingredients. You can make #{serving_size_mod/7} more pie(s) plus #{serving_size_mod % 7} more cookies."
     elsif serving_size_mod >= 5
-     p "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}. You have #{serving_size_mod} leftover ingredient. You can have #{serving_size_mod/5} more cake(s) plus #{serving_size_mod % 5} more cookies."
+     p "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}. You have #{serving_size_mod} leftover ingredients. You can have #{serving_size_mod/5} more cake(s) plus #{serving_size_mod % 5} more cookies."
     else
-      p "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}. You have #{serving_size_mod} leftover ingredient. You can have #{serving_size_mod/1} more cookie(s)."
+      p "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}. You have #{serving_size_mod} leftover ingredients. You can have #{serving_size_mod/1} more cookie(s)."
   end
 end
 
@@ -31,8 +31,22 @@ p serving_size_calc("THIS IS AN ERROR", 5)
 
 #  Reflection
 
-What did you learn about making code readable by working on this challenge?
-Did you learn any new methods? What did you learn about them?
-What did you learn about accessing data in hashes?
-What concepts were solidified when working through this challenge?
+# What did you learn about making code readable by working on this challenge?
+# Did you learn any new methods? What did you learn about them?
+# What did you learn about accessing data in hashes?
+# What concepts were solidified when working through this challenge?
+
+=begin
+Readable code matters. Otherwise simple programs can become almost
+unintelligible without concise, eloquent code. Simple is sometimes better.
+I learned about the .values_at method, which will return the values of
+select keys in a hash. I also learned that, when iterating over a hash,
+if you select just one item between the "pipes," then it will iterate
+over the entire key-value pair, e.g., hash.each {|food| p food} iterates over
+each key-value pair, not just the value or the key.
+I feel comfortable with control flow and IF-ELSE constructions, which my
+pair and I used extensively here.
+=end
+
+
 
