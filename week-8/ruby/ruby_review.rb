14fa5afc7713f6ce8eae73bb8d3417f3bbb6e7f7
reginafcompton <reginafcompton@gmail.com>
2015-10-07 21:48:03 -0500

    Finalize pizza car solution

--- week-8/ruby/ruby_review.rb
+++ week-8/ruby/ruby_review.rb
@@ -11,7 +11,7 @@
     # ELSIF the integer is divisible by 5 and 3 replace it with "buzz"
     # ELSE the integer remains unchanged
 
-Solution FizzBuzz
+# Solution FizzBuzz
 
 def super_fizzbuzz(array)
   array.map! {|i|
@@ -210,7 +210,7 @@ num_to_words(5)
 # Input: a string
 # Output: a string with each word reversed
 
-# Put the indiviual words of the string into a collection (an array).
+# Put the individual words of the string into a collection (an array).
 # Iterate over the collection, and reverse each element.
 # Join together the elements in the collection into a string.
 
@@ -226,7 +226,180 @@ def reverse_words(string)
 end
 
 # Driver Code
-reverse_words("An apple a day keeps the docotor away")
+reverse_words("An apple a day keeps the doctor away")
 reverse_words("")
 
+
+
+# Pseudocode Create a Car Class
+# Create a class called Car.
+# Give the Car certain characteristics: model, color, distance to drive, and starting speed.
+# That is, initialize the class with instance variables.
+# Define a method for turning left or right.
+# Define a method for checking the distance traversed.
+# Define a method for stopping; this method sets the speed of the car to zero.
+# Define a method that shows a play-by-play of the car's movements.
+
+
+# Solution
+class Car
+  attr_accessor :model, :color, :pizzas, :target_distance, :travel_distance, :speed, :progress
+
+  def initialize(model, color)
+    @model = model
+    @color = color
+    @pizzas = []
+    @target_distance = target_distance
+    @travel_distance = 0.0
+    @speed = 0
+    @progress = []
+  end
+
+  def target(miles)
+    @target_distance = miles.to_f
+  end
+
+  def drive(num)
+    @travel_distance += num.to_f
+    @progress << "travel #{num} miles"
+  end
+
+  def right
+    p "You turned right."
+    @progress << "right turn"
+  end
+
+  def left
+    p "You turned left."
+    @progress << "left turn"
+  end
+
+  def accelerate
+    @speed += 5
+    p "You accelerated 5 mph."
+    @progress << "accelerate"
+  end
+
+  def decelerate
+    @speed -= 5
+    p "You decelerated 5 mph."
+    @progress << "decelerate"
+  end
+
+  def stop
+    @speed = 0
+    p "You stopped."
+    @progress << "stop"
+  end
+
+  def speed
+    p "You are going #{@speed} MPH!"
+  end
+
+  def distance
+    remaining_miles = @target_distance - @travel_distance
+    if remaining_miles > 0.0
+      p "You traveled #{@travel_distance}!"
+      p "#{remaining_miles} miles remain."
+    elsif remaining_miles == 0.0
+      p "You arrived at your destination!"
+    else remaining_miles < 0.0
+      p "You went too far. Turn around!"
+    end
+  end
+
+  def pizzas_to_deliver(pizza)
+    @pizzas << pizza.to_s
+  end
+
+  def delivery
+    p "Enjoy your #{@pizzas[0]}!"
+    @pizzas.shift
+  end
+end
+
+class Pizza
+  attr_accessor :size, :toppings, :toppings
+
+  def initialize(size, toppings, price)
+    @size = size
+    @price = price
+    @toppings = toppings
+  end
+
+  def order
+    p "Thank you for your order: a #{@size} pizza with #{@toppings}."
+    p "Please pay the delivery person #{@price} dollars plus a tip."
+  end
+
+  def to_s
+    "#{@size} pizza with #{@toppings} for #{@price} dollars"
+  end
+
+end
+
+
+# DRIVER TESTS
+
+driver = Car.new("Fiat", "red")
+driver.target(3.0)
+driver.drive(0.25)
+driver.accelerate
+driver.accelerate
+driver.accelerate
+driver.accelerate
+driver.stop
+driver.right
+driver.drive(1.5)
+driver.accelerate
+driver.accelerate
+driver.accelerate
+driver.accelerate
+driver.accelerate
+driver.speed
+driver.decelerate
+driver.decelerate
+driver.drive(0.25)
+driver.stop
+driver.left
+driver.drive(1.4)
+driver.accelerate
+driver.accelerate
+driver.accelerate
+driver.accelerate
+driver.accelerate
+driver.distance
+
+pizza1 = Pizza.new("small", "sausage", 8)
+pizza2 = Pizza.new("large", "mushrooms", 15)
+pizza3 = Pizza.new("medium", "peppers", 11)
+driver.pizzas_to_deliver(pizza1)
+driver.pizzas_to_deliver(pizza2)
+driver.pizzas_to_deliver(pizza3)
+driver.delivery
+driver.delivery
+driver.delivery
+
+
+
 # Reflection
+
+# What concepts did you review or learn in this challenge?
+=begin
+I reviewed control flow: using IF statements and also CASE statements.
+I learned a new method – .strip – which removes blank "white space" at the front and back of a string.
+=end
+
+
+# What is still confusing to you about Ruby?
+=begin
+I do not know if "confusing" is the right word: I do feel that Ruby offers a lot, and I do not yet know the full gamut of possibilities.
+=end
+
+
+# What are you going to study to get more prepared for Phase 1?
+=begin
+I am building a WordDoc with lots of examples and minutiae that I cannot keep in my little head.
+I am also re-reading all my notes, focusing especially on Ruby (Weeks 4-6).
+I am also building a new website for fun, just to keep my CSS and HTML chops in shape.
+=end
